f1687da769a54db2d49ca607a1343ebe78ab65e1
zczhao1992 <zczhao1992@163.com>
2023-06-07 20:10:02 +0800

    增加基础样式

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import styles from "./QuestionCard.module.scss";
+
+const QuestionCard: FC = () => {
+    return (
+        <>
+            <p>List</p>
+        </>
+    );
+};
+
+export default QuestionCard;
---
+++ src/pages/List.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState } from "react";
+import QuestionCard from "../components/QuestionCard";
+import styles from "./List.module.scss";
+
+const rawQuestionList = [
+    {
+        id: "q1",
+        title: "问卷1",
+        isPublished: false,
+        isStar: false,
+        answerCount: 5,
+        createAt: "3月10日 12:23",
+    },
+    {
+        id: "q2",
+        title: "问卷2",
+        isPublished: true,
+        isStar: false,
+        answerCount: 5,
+        createAt: "3月10日 12:23",
+    },
+    {
+        id: "q3",
+        title: "问卷3",
+        isPublished: false,
+        isStar: false,
+        answerCount: 5,
+        createAt: "3月10日 12:23",
+    },
+    {
+        id: "q4",
+        title: "问卷4",
+        isPublished: true,
+        isStar: false,
+        answerCount: 5,
+        createAt: "3月10日 12:23",
+    },
+]
+
+const List: FC = () => {
+    const [questionList, setQuestionList] = useState(rawQuestionList);
+
+    return (
+        <>
+            <p>List</p>
+        </>
+    );
+};
+
+export default List;
